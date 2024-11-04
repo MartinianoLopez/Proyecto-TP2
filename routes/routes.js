2f41d246bd454953cc170e644f35b1c06e9c18d1
Agostina Braga <agostinabraga@gmail.com>
2024-11-03 21:13:28 -0300

    arreglo en routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes.js";
+import resenasRoutes from "./resenasRoutes.js";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
+routes.use("/resenas", resenasRoutes); 
 // routes.use("/roles",rolesRoutes);
 
 
